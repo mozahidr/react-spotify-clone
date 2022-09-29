ebe1dc1f7bb64fe4e340fd232d79d0cf7936e0c0
Mozahid RANA <mozahidpic@gmail.com>
2022-09-30 05:20:17 +1000

    first commit

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+    
+export const shazamCoreApi = createApi({
+        reducerPath: 'shazamCoreApi',
+        baseQuery: fetchBaseQuery({
+            baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+            prepareHeaders: (headers) => {
+                headers.set('X-RapidAPI-Key', '3dad213a05msh8a6e147d68b85bep1516bfjsnfacbeff8e1f5');
+
+                return headers;
+            },
+        }),
+        endpoints: (builder) => ({
+            getTopCharts: builder.query({ query: () => '/charts/world' }),
+            getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
+            getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+            getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+            getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+            getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+            getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+
+
+        }),
+    });
+
+    export const {
+        useGetTopChartsQuery,
+        useGetSongDetailsQuery,
+        useGetSongRelatedQuery,
+        useGetArtistDetailsQuery,
+        useGetSongsByCountryQuery,
+        useGetSongsByGenreQuery,
+        useGetSongsBySearchQuery,
+    } = shazamCoreApi;
